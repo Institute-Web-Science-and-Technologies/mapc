382cbed029b031d0a9074349fd42fdce83501011
0nse <0nse@users.noreply.github.com>
2014-06-03 11:14:13 +0200

    - add code formatting as save action - changed Agent recharge Action to constant - modified executeAction to return a bool whether it failed

--- implementation/mako/src/java/eis/ActionHandler.java
+++ implementation/mako/src/java/eis/ActionHandler.java
@@ -10,15 +10,17 @@ import eis.iilang.Identifier;
 public class ActionHandler {
 	/**
 	 * Yields a valid goto-action.
+	 * 
 	 * @param nodeName
 	 * @return
 	 */
 	static public Action goTo(String nodeName) {
-		return new Action("goto",new Identifier(nodeName));
+		return new Action("goto", new Identifier(nodeName));
 	}
 
 	/**
 	 * Yields a valid skip-action.
+	 * 
 	 * @return
 	 */
 	static public Action skip() {
@@ -27,14 +29,16 @@ public class ActionHandler {
 
 	/**
 	 * Yields a valid probe-action.
+	 * 
 	 * @return
 	 */
 	static public Action probe() {
 		return new Action("probe");
 	}
-	
+
 	/**
 	 * Yields a valid probe-action for a specific node.
+	 * 
 	 * @return
 	 */
 	static public Action probe(String nodeName) {
@@ -43,6 +47,7 @@ public class ActionHandler {
 
 	/**
 	 * Yields a valid survey-action.
+	 * 
 	 * @return
 	 */
 	static public Action survey() {
@@ -51,14 +56,16 @@ public class ActionHandler {
 
 	/**
 	 * Yields a valid inspect-action.
+	 * 
 	 * @return
 	 */
 	static public Action inspect() {
 		return new Action("inspect");
 	}
-	
+
 	/**
 	 * Yields a valid inspect-action for a specific agent.
+	 * 
 	 * @return
 	 */
 	static public Action inspect(String agentName) {
@@ -67,46 +74,49 @@ public class ActionHandler {
 
 	/**
 	 * Yields a valid parry-action.
+	 * 
 	 * @return
 	 */
 	public static Action parry() {
 		return new Action("parry");
 	}
-	
+
 	/**
 	 * Yields a valid attack action.
+	 * 
 	 * @param entityName
 	 * @return
 	 */
 	static public Action attack(String entityName) {
-		return new Action("attack",new Identifier(entityName));
+		return new Action("attack", new Identifier(entityName));
 	}
 
 	/**
 	 * Yields a valid buy action.
-	 * @param item 
+	 * 
+	 * @param item
 	 * @return
 	 */
 	static public Action buy(String item) {
-		return new Action("buy",new Identifier(item));
+		return new Action("buy", new Identifier(item));
 	}
 
 	/**
 	 * Yields a valid repair action.
+	 * 
 	 * @param entity
 	 * @return
 	 */
 	static public Action repair(String entity) {
-		return new Action("repair",new Identifier(entity));
+		return new Action("repair", new Identifier(entity));
 	}
-	
+
 	/**
 	 * Yields a valid recharge action.
+	 * 
 	 * @param entity
 	 * @return
 	 */
-	static public Action recharge() {
-		return new Action("recharge");
-	}
+	public final static Action RECHARGE = new Action("recharge");
 
 }
--- implementation/mako/src/java/eis/EISEnvironment.java
+++ implementation/mako/src/java/eis/EISEnvironment.java
@@ -4,10 +4,11 @@
  */
 package eis;
 
-import java.io.IOException;
-
 import jason.asSyntax.Structure;
 import jason.environment.Environment;
+
+import java.io.IOException;
+
 import c4jason.CartagoEnvironment;
 import eis.exceptions.ManagementException;
 
@@ -16,8 +17,9 @@ public class EISEnvironment extends Environment {
 	private EnvironmentInterfaceStandard ei;
 	private CartagoEnvironment cartagoEnvironment;
 	private AgentHandler agentHandler;
-	
-	private AgentLogger logger = new AgentLogger(this.getClass().getCanonicalName());
+
+	private AgentLogger logger = new AgentLogger(this.getClass()
+			.getCanonicalName());
 
 	/*
 	 * jason lifecycle: init -> user-init -> compile -> run -> user-end
@@ -26,7 +28,8 @@ public class EISEnvironment extends Environment {
 	public void init(String[] args) {
 		// init EISMASSIM environment
 		try {
-			ei = EILoader.fromClassName("massim.eismassim.EnvironmentInterface");
+			ei = EILoader
+					.fromClassName("massim.eismassim.EnvironmentInterface");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -59,13 +62,17 @@ public class EISEnvironment extends Environment {
 			}
 		}
 	}
-	
+
 	@Override
-	  public boolean executeAction(String agName, Structure action) {
-	    if (action.getFunctor().equals("recharge")) {
-	    	agentHandler.getAgent(agName).doAction(ActionHandler.recharge());
-	    	logger.info(agName+": i wanna recharge");
-	  }
-	    return true;
+	public boolean executeAction(String agName, Structure action) {
+		if (action.getFunctor().equals("recharge")) {
+			boolean actionResult = agentHandler.getAgent(agName).doAction(
+					ActionHandler.RECHARGE);
+			if (actionResult)
+				logger.info(agName + "I recharged");
+			else
+				logger.info(agName + "I cannot recharge");
+		}
+		return true;
 	}
 }
