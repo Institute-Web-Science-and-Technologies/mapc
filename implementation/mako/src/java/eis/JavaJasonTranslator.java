5e5fffe19cbecfc4ecb09f247f7dfb41fcc22327
Michael Sewell <sewell@uni-koblenz.de>
2014-06-06 14:03:33 +0200

    Added rudimentary Java->Jason translator class that probably doesn't work.

---
+++ implementation/mako/src/java/eis/JavaJasonTranslator.java
@@ -0,0 +1,18 @@
+package eis;
+
+import jason.asSyntax.Literal;
+import eis.iilang.Percept;
+
+/**
+ * This class adds the functionality to translate between Java percepts and
+ * Jason literals.
+ * 
+ * @author Michael Sewell
+ * 
+ */
+public class JavaJasonTranslator {
+    public Literal perceptToLiteral(Percept percept) {
+        Literal literal = Literal.parseLiteral(percept.toString());
+        return literal;
+    }
+}
