f2333e4878d597ff14097e98e0b3cf8e77edc21f
Michael Sewell <sewell@uni-koblenz.de>
2014-08-22 13:02:51 +0200

    Implemented internal action getEdgeCosts

--- implementation/mako/src/java/ia/getEdgeCosts.java
+++ implementation/mako/src/java/ia/getEdgeCosts.java
@@ -2,25 +2,25 @@
 
 package ia;
 
-import jason.JasonException;
 import jason.asSemantics.DefaultInternalAction;
 import jason.asSemantics.TransitionSystem;
 import jason.asSemantics.Unifier;
 import jason.asSyntax.Term;
+import eis.JasonHelper;
+import eis.MapAgent;
+import eis.Vertex;
 
 // Call from AgentSpeak: getEdgeCosts(Position, Destination, Costs) 
 public class getEdgeCosts extends DefaultInternalAction {
 
+    private static final long serialVersionUID = 7036124114716986040L;
+
     @Override
     public Object execute(TransitionSystem ts, Unifier un, Term[] args)
             throws Exception {
-        // execute the internal action
-        ts.getAg().getLogger().info("executing internal action 'ia.getEdgeCosts'");
-        if (true) { // just to show how to throw another kind of exception
-            throw new JasonException("not implemented!");
-        }
-
-        // everything ok, so returns true
-        return true;
+        Vertex position = MapAgent.getInstance().getVertex(args[0].toString());
+        Vertex destination = MapAgent.getInstance().getVertex(args[1].toString());
+        int costs = position.getPath(destination).getPathCosts();
+        return un.unifies(args[1], JasonHelper.getTerm(costs));
     }
 }
