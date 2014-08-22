222b2a05de61259ca9b7ead3f307510c62aa264b
Artur Daudrich <daudrich.artur@googlemail.com>
2014-08-22 11:13:58 +0200

    optimized internal action isProbed

--- implementation/mako/src/java/ia/isProbed.java
+++ implementation/mako/src/java/ia/isProbed.java
@@ -6,11 +6,10 @@ import jason.asSemantics.DefaultInternalAction;
 import jason.asSemantics.TransitionSystem;
 import jason.asSemantics.Unifier;
 import jason.asSyntax.Term;
-import eis.JasonHelper;
 import eis.MapAgent;
 import eis.Vertex;
 
-//Call from AgentSpeak: isProbed(Vertex, Result)
+//Call from AgentSpeak: isProbed(Vertex)
 public class isProbed extends DefaultInternalAction {
     private static final long serialVersionUID = 2385104427608117167L;
 
@@ -18,10 +17,6 @@ public class isProbed extends DefaultInternalAction {
     public Object execute(TransitionSystem ts, Unifier unifier, Term[] terms)
             throws Exception {
         Vertex vertex = MapAgent.getInstance().getVertex(terms[0].toString());
-        Term resultTerm = terms[1];
-
-        boolean result = MapAgent.getInstance().isVertexProbed(vertex);
-
-        return unifier.unifies(resultTerm, JasonHelper.getTerm(result));
+        return MapAgent.getInstance().isVertexProbed(vertex);
     }
 }
