54a526da74bbc795f76d1dd09638b4423985154e
Artur Daudrich <daudrich.artur@googlemail.com>
2014-08-22 11:14:21 +0200

    optimized internal action isSurveyed

--- implementation/mako/src/java/ia/isSurveyed.java
+++ implementation/mako/src/java/ia/isSurveyed.java
@@ -6,11 +6,10 @@ import jason.asSemantics.DefaultInternalAction;
 import jason.asSemantics.TransitionSystem;
 import jason.asSemantics.Unifier;
 import jason.asSyntax.Term;
-import eis.JasonHelper;
 import eis.MapAgent;
 import eis.Vertex;
 
-//Call from AgentSpeak: isSurveyed(Vertex, Result)
+//Call from AgentSpeak: isSurveyed(Vertex)
 public class isSurveyed extends DefaultInternalAction {
     private static final long serialVersionUID = 1410005108812986441L;
 
@@ -18,10 +17,6 @@ public class isSurveyed extends DefaultInternalAction {
     public Object execute(TransitionSystem ts, Unifier unifier, Term[] terms)
             throws Exception {
         Vertex vertex = MapAgent.getInstance().getVertex(terms[0].toString());
-        Term resultTerm = terms[1];
-
-        boolean result = MapAgent.getInstance().isVertexSurveyed(vertex);
-
-        return unifier.unifies(resultTerm, JasonHelper.getTerm(result));
+        return MapAgent.getInstance().isVertexSurveyed(vertex);
     }
 }
