c9bee116c6a406c6312177ee3fe914440ce11024
Michael Sewell <sewell@uni-koblenz.de>
2014-08-29 11:43:06 +0200

    Added equals() and hashCode() to Vertex
    This should solve bug #31 once and for all.

--- implementation/mako/src/java/eis/Vertex.java
+++ implementation/mako/src/java/eis/Vertex.java
@@ -324,4 +324,39 @@ public class Vertex {
         }
     }
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((identifier == null) ? 0 : identifier.hashCode());
+        return result;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Vertex other = (Vertex) obj;
+        if (identifier == null) {
+            if (other.identifier != null)
+                return false;
+        } else if (!identifier.equals(other.identifier))
+            return false;
+        return true;
+    }
+
 }
