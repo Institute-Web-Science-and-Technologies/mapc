6b6a9fbc01597a92462c32f5ad1fe28efbcbff3a
0nse <0nse@users.noreply.github.com>
2014-09-10 10:31:29 +0200

    Fix: #39 by making cancelling of zones atomic. New: Removed logging in getBestZone as I am not interested in this information. Comment: I'm not sure if #39 should have ever arised and if it is fought completely by now. Closing it for now nevertheless.

--- implementation/mako/src/java/ia/getBestZone.java
+++ implementation/mako/src/java/ia/getBestZone.java
@@ -9,7 +9,6 @@ import jason.asSyntax.Term;
 
 import java.util.List;
 
-import eis.AgentLogger;
 import eis.JasonHelper;
 import eis.MapAgent;
 import eis.Vertex;
@@ -18,12 +17,13 @@ import eis.Zone;
 //Call from AgentSpeak: getBestZone(Position, Range, ZoneValuePerAgent, CenterVertex, ListOfAgents)
 public class getBestZone extends DefaultInternalAction {
     private static final long serialVersionUID = -6937681288781906625L;
-    private AgentLogger logger;
+
+    // private AgentLogger logger;
 
     @Override
     public Object execute(TransitionSystem ts, Unifier un, Term[] args)
             throws Exception {
-        logger = new AgentLogger("getBestZone");
+        // logger = new AgentLogger("getBestZone");
         Term zoneValuePerAgent = args[2];
         Term centerVertex = args[3];
         Term listOfAgents = args[4];
@@ -32,9 +32,11 @@ public class getBestZone extends DefaultInternalAction {
         int range = Integer.parseInt(args[1].toString());
         Vertex vertex = mapAgent.getVertex(args[0].toString());
         Zone zone = mapAgent.getBestZone(mapAgent.getZonesInRange(vertex, range));
-        logger.info("Vertex: " + vertex + " Range: " + range);
+        // logger.info("Vertex: " + vertex + " Range: " + range);
         if (zone != null) {
-            logger.info("getBestZone: " + zone.getPositions() + " ZoneCenter: " + zone.getCenter() + "MyPosition: " + vertex + "Range: " + range);
+            // logger.info("getBestZone: " + zone.getPositions() +
+            // " ZoneCenter: " + zone.getCenter() + "MyPosition: " + vertex +
+            // "Range: " + range);
             List<String> closestAgents = mapAgent.getClosestAgentsToZone(zone.getCenter(), zone.getPositions().size());
             if (closestAgents.size() > 0) {
                 Double zoneValue = zone.getZoneValuePerAgent() / closestAgents.size();
@@ -42,10 +44,11 @@ public class getBestZone extends DefaultInternalAction {
                 un.unifies(centerVertex, JasonHelper.getTerm(zone.getCenter().getIdentifier()));
                 un.unifies(listOfAgents, JasonHelper.getStringList(closestAgents));
             }
-            logger.info("getBestZone closest Agents: " + closestAgents);
+            // logger.info("getBestZone closest Agents: " + closestAgents);
             return closestAgents.size() == zone.getPositions().size();
         }
-        logger.info("getBestZone: " + zone + " MyPosition: " + vertex + "Range: " + range);
+        // logger.info("getBestZone: " + zone + " MyPosition: " + vertex +
+        // "Range: " + range);
         return false;
     }
 
